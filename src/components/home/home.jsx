2e8d28c369b9db36644387aec5780d6c5aa39043
milkdue <321432896@qq.com>
2020-10-11 11:42:09 +0800

    api update

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,12 +1,28 @@
 import React, {Component} from 'react';
-
+import { Carousel } from 'antd';
+import '../css/home.less';
 
 export default class Home extends Component{
 
 
     render(){
         return (
-            <div>Home</div>
+            <div> 
+                <Carousel className="autoplay" autoplay>
+                    <div>
+                        <h2 className="autoplay">欢迎使用商品管理系统</h2>
+                    </div>
+                    <div>
+                        <h2 className="autoplay">欢迎使用商品管理系统</h2>
+                    </div>
+                    <div>
+                        <h2 className="autoplay">欢迎使用商品管理系统</h2>
+                    </div>
+                    <div>
+                        <h2 className="autoplay">欢迎使用商品管理系统</h2>
+                    </div>
+                </Carousel>
+          </div>
         )
     }
 }
