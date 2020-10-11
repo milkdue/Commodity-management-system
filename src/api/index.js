3f456105a9f04f387b9e23be60ad4194111a326f
milkdue <321432896@qq.com>
2020-10-11 15:00:32 +0800

    add user

--- src/api/index.js
+++ src/api/index.js
@@ -50,4 +50,4 @@ export const reqAuthRole = (obj) => myAxios.post(`${BASE_URL}/manage/role/update
 // 请求用户列表
 export const reqUserList = () => myAxios.get(`${BASE_URL}/manage/user/list`);
 // 添加一个用户
-export const reqAddUser = (obj) => myAxios.post(`${BASE_URL}/manage/user/add`, {...obj, auth_time: Date.now()});
+export const reqAddUser = (obj) => myAxios.post(`${BASE_URL}/manage/user/add`, {...obj});
--- src/containers/user/user.jsx
+++ src/containers/user/user.jsx
@@ -1,39 +1,71 @@
 import React, {Component} from 'react';
-import {Card, Button, Table} from 'antd';
+import {Card, Button, Table, Modal, Form, Input, Select, message} from 'antd';
 import {UserAddOutlined} from '@ant-design/icons';
 import dayjs from 'dayjs';
-import {reqUserList} from '../../api/index.js';
+import {reqUserList, reqAddUser} from '../../api/index.js';
+const {Option} = Select;
 
 
 export default class User extends Component{
     state = {
-        isLoading: false,
+        isLoading: true, // 加载
+        addVisible: false, // 添加用户模态框
+        roles: [], // 所有角色名
+        users: [] // 所有用户信息
     }
 
     componentDidMount(){
         this.getUserList()
     }
-
+    // 获取所有用户列表
     getUserList = async () => {
         let result = await reqUserList();
-        console.log(result);
+        if(result.status === 0){
+            let {users, roles} = result.data;
+            this.setState({roles, users: users.reverse()})
+        }else{
+            message.error('获取列表失败,请刷新!', 1);
+        }
+        this.setState({isLoading: false});
     }
+    // 
+    showAddModal = () => {
+        this.setState({
+            addVisible: true,
+        });
+    };
+
+    handleAddOk = () => {
+        // 表单验证
+        this.formRef.validateFields()
+        .then(value => {
+            reqAddUser(value).then(result => {
+                if(result.status === 0){
+                    message.success('添加成功!', 1);
+                    this.getUserList()
+                }else{
+                    message.warning(result.msg + '!', 1);
+                }
+            })
+        })
+        .catch(error => message.error(error.message, 1))
+        this.setState({
+            addVisible: false,
+        });
+    };
+
+    handleAddCancel = () => {
+        this.setState({
+            addVisible: false,
+        });
+    };
+    // 选择框
+    handleChange = () => {
+
+    }
+
     render(){
-        const dataSource = [
-            {
-              _id: '1',
-              name: '胡彦斌',
-              age: 32,
-              address: '西湖区湖底公园1号',
-            },
-            {
-              _id: '2',
-              name: '胡彦祖',
-              age: 42,
-              address: '西湖区湖底公园1号',
-            },
-          ];
-          
+        const {isLoading, addVisible, roles, users} = this.state;
         const columns = [
             {
                 title: '用户名',
@@ -58,13 +90,14 @@ export default class User extends Component{
                 dataIndex: 'create_time',
                 align: 'center',
                 key: 'create_time',
-                render: (time) => dayjs(time).format('YYYY-MM-DD HH:mm:ss')
+                render: time => dayjs(time).format('YYYY-MM-DD HH:mm:ss')
             },
             {
                 title: '所属角色',
                 dataIndex: 'role_id',
                 align: 'center',
-                key: 'role_id',
+                key: 'roleName',
+                render: id => roles.find((item => item._id === id)).name
             },
             {
                 title: '操作',
@@ -80,11 +113,132 @@ export default class User extends Component{
                 }
             },
         ];
-        const {isLoading} = this.state;
         return (
             <div>
-                <Card title={<Button type="primary"><UserAddOutlined />创建用户</Button>}>
-                    <Table dataSource={dataSource} columns={columns} bordered rowKey="_id" loading={isLoading}/>
+                <Modal
+                    title="用户添加"
+                    visible={addVisible}
+                    onOk={this.handleAddOk}
+                    onCancel={this.handleAddCancel}
+                    okText="确认"
+                    cancelText="取消"
+                >
+                    <Form 
+                            name="normal_login" 
+                            className="login-form" 
+                            labelCol={{xs: 8, md: 6, lg: 4}}
+                            ref={(form) => {this.formRef = form}}
+                        >
+                        <Form.Item
+                            name="username"
+                            label="用户名"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '用户名不能为空!'
+                                },
+                                {
+                                    min: 4,
+                                    message: '用户名必须大于等于4位!'
+                                },
+                                {
+                                    max: 12,
+                                    message: '用户名必须小于等于12位!'
+                                },
+                                {
+                                    pattern: /^\w+$/,
+                                    message: '用户名必须由字母数字下划线组成!'
+                                }
+                            ]}
+                        >
+                            <Input
+                                placeholder="请输入用户名"
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            label="密码"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '密码不能为空!'
+                                },
+                                {
+                                    min: 5,
+                                    message: '密码必须大于等于5位!'
+                                },
+                                {
+                                    max: 18,
+                                    message: '密码必须小于等于18位!'
+                                },
+                                {
+                                    pattern: /^\w+$/,
+                                    message: '密码必须由字母数字下划线组成!'
+                                }
+                            ]}
+                        >
+                            <Input
+                                placeholder="请输入密码"
+                                type="password"
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="phone"
+                            label="电话号码"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '电话号码不能为空!'
+                                },
+                                {
+                                    pattern: /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/,
+                                    message: '请输入正确格式的电话号码!',
+                                }
+                            ]}
+                        >
+                            <Input
+                                placeholder="请输入电话号码"
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="email"
+                            label="邮箱"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '邮箱不能为空!'
+                                },
+                                {
+                                    type: 'email',
+                                    message: '请输入正确格式的邮箱!'
+                                }
+                            ]}
+                        >
+                            <Input
+                                type="email"
+                                placeholder="请输入邮箱"
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="role_id"
+                            label="角色"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '请选择一个角色!'
+                                }
+                            ]}
+                        >
+                            <Select onChange={this.handleChange} placeholder="请选择一个角色">
+                                {
+                                    roles.map((item) => (<Option value={item._id} key={item._id}>{item.name}</Option>))
+                                }
+                            </Select>
+                        </Form.Item>
+                    </Form>
+                </Modal>
+                <Card title={<Button type="primary" onClick={this.showAddModal}><UserAddOutlined />创建用户</Button>}>
+                    <Table dataSource={users} columns={columns} bordered rowKey="_id" loading={isLoading}/>
                 </Card>
             </div>
         )
